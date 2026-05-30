8e1e1547d6d102a24f9d6be5751d8d943393b3fd
bcalderonflorez-byu <bcalderonflorez@byupathway.edu>
2026-05-30 12:17:42 -0500

    week03 - MVC implementation

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
-import { getAllOrganizations } from './src/models/organizations.js';
-import { getAllProjects } from './src/models/projects.js';
-import { getAllCategories } from './src/models/categories.js';
-
+import router from './src/routes.js';
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
@@ -48,41 +45,10 @@ app.use((req, res, next) => {
 /**
  * Routes
  */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
-});
 
-app.get('/organizations', async (req, res) => {
-    const organizations = await getAllOrganizations();
-    const title = 'Our Partner Organizations';
+// Use the imported router to handle routes
+app.use(router);
 
-    res.render('organizations', { title, organizations });
-});
-
-app.get('/projects', async (req, res) => {
-    const projects = await getAllProjects();
-    console.log(projects);
-    const title = 'Service Projects';
-
-    res.render('projects', { title, projects });
-});
-
-
-app.get('/categories', async (req, res) => {
-    const categories = await getAllCategories();
-    console.log(categories);
-    const title = 'Categories';
-
-    res.render('categories', { title, categories });
-});
-
-// Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
-    const err = new Error('This is a test error');
-    err.status = 500;
-    next(err);
-});
 
 // Catch-all route for 404 errors
 app.use((req, res, next) => {
